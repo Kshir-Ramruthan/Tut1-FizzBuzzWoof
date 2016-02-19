523e6d36482aea54ddec405023f2cdd9abf3c4b4
Kshir Ramruthan <kshirramruthan@gmail.com>
2016-02-19 10:19:53 +0200

    Fizz, Buzz and Woof

--- Program.cpp
+++ Program.cpp
@@ -7,20 +7,36 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 20; i++)
+	for (int i = 1; i <= 50; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+		{
+			cout << "Fizz Buzz Woof" << endl;
+		}//End if
+		else if (i % 3 == 0 && i%5==0)
 		{
 			cout << "Fizz Buzz" << endl;
-		}//End if
+		}//End 1st else if
+		else if (i % 3 == 0 && i % 7 == 0)
+		{
+			cout << "Fizz Woof" << endl;
+		}//End 2nd else if
+		else if (i % 5 == 0 && i % 7 == 0)
+		{
+			cout << "Buzz Woof" << endl;
+		}//End 3rd else if
 		else if (i % 3 == 0)
 		{
 			cout << "Fizz" << endl;
-		}//End 1st else if
+		}//End 4th else if
 		else if (i % 5 == 0)
 		{
 			cout << "Buzz" << endl;
-		}//End 2nd else if
+		}//End 5th else if
+		else if (i % 7 == 0)
+		{
+			cout << "Woof" << endl;
+		}//End 6th else if
 		else
 		{
 			cout << i << endl;
